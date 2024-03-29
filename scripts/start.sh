f2c40306f6a6bd531291e8b57a36d5a4f9a9e03c
Vitalij Lysov <vklysoff@gmail.com>
2024-03-29 16:05:27 +0300

    * Remove internal DB * Logs to stdout * Define SOPDS_FB2TOXHTML * Add TODO

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,53 +1,34 @@
 #!/bin/bash
 
-waiting_db(){
-    while ! pg_isready -U postgres > /dev/null; do
-        echo "$(date) - waiting for database to start"
-        sleep 10
-    done
-}
-
-if ! [ -d /run/postgresql ]; then
-    mkdir -p /run/postgresql
-    chown -R postgres:postgres /run/postgresql
-fi
-
-if [[ $EXT_DB == False && ! -f /var/lib/pgsql/data/PG_VERSION ]]; then
-    chown -R postgres:postgres /var/lib/pgsql
-    chmod -R 0700 /var/lib/pgsql
-    su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data initdb"
-    su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log start"
-    waiting_db
-    psql -U postgres -c "create database sopds"
-    psql -U postgres -c "create user sopds with password 'sopds'"
-    psql -U postgres -c "grant all privileges on database sopds to sopds"
+if [ $MIGRATE == True ]; then
     cd /sopds
     python3 manage.py migrate
-    su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log stop"
 fi
-if [ $EXT_DB == False ]; then
-    su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log start"
-    waiting_db
-fi
-cd /sopds
-if [ $MIGRATE == True ]; then
-    python3 manage.py migrate
-fi
-if [ ! -f /var/lib/pgsql/setconf ]; then
+
+# TODO: Надо флаг /sopds/setconf в другое место поместить
+if [ ! -f /sopds/setconf ]; then
+    cd /sopds
     python3 manage.py sopds_util setconf SOPDS_ROOT_LIB $SOPDS_ROOT_LIB
     python3 manage.py sopds_util setconf SOPDS_INPX_ENABLE $SOPDS_INPX_ENABLE
     python3 manage.py sopds_util setconf SOPDS_LANGUAGE $SOPDS_LANGUAGE
 
+    # Logs to stdout
+    python3 manage.py sopds_util setconf SOPDS_SERVER_LOG "/dev/stdout"
+    python3 manage.py sopds_util setconf SOPDS_SCANNER_LOG "/dev/stdout"
+    python3 manage.py sopds_util setconf SOPDS_TELEBOT_LOG "/dev/stdout"
+
+
     #configure fb2converter for epub and mobi - https://github.com/rupor-github/fb2converter
     python3 manage.py sopds_util setconf SOPDS_FB2TOEPUB "convert/fb2c/fb2epub"
     python3 manage.py sopds_util setconf SOPDS_FB2TOMOBI "convert/fb2c/fb2mobi"
+    python3 manage.py sopds_util setconf SOPDS_FB2TOXHTML "convert/fb2xhtml/fb2xhtml"
 
     #autocreate the superuser
     if [[ ! -z $SOPDS_SU_NAME && ! -z $SOPDS_SU_EMAIL &&  ! -z $SOPDS_SU_PASS ]]; then
         expect /sopds/superuser.exp
     fi
 
-    touch /var/lib/pgsql/setconf
+    touch /sopds/setconf
 fi
 
 #To start the Telegram-bot if it enabled
